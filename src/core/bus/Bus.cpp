3aba2f3ba3f3378a3278a32b10db22db788ef9e6
Dadeathmag <mohammedramshad246@gmail.com>
2026-06-28 09:52:13 +0000

    implemented mapper as a seperate funtion

--- src/core/bus/Bus.cpp
+++ src/core/bus/Bus.cpp
@@ -1,105 +1,70 @@
 #include "Bus.hpp"
 
 uint8_t Bus::readBus(uint16_t addr){
-    
-    switch(addr>>12){
-        case 0x0:
-        case 0x1:
-        case 0x2:
-        case 0x3:
-            //Rom bank 0
-            break;
-        case 0x4:
-        case 0x5:
-        case 0x6:
-        case 0x7:
-            //switchable Rom bank 1 to N (managed by cartridge) 
-            break;
-        case 0x8:
-        case 0x9:
-            //Vram (DMG:has only bank,CGB:can switch BtW 0/1)
-            break;
-        case 0xA:
-        case 0xB:
-            //External Ram from cartridge(switchable)
-            break;
-        case 0xC:
-            //Work Ram bank 0   (Wram Partition only applicable to CGB)  
-        case 0xD:
-            //Work Ram bank 1-7 (Wram Partition only applicable to CGB)
-            break;
-        case 0xE://upto FDFF
-            //Echo RAM (mirror of C000–DDFF) prohibited
-            break;
-        case 0xF:
-            if (addr <= 0xFDFF){
-                // Echo RAM (mirror of C000–DDFF) prohibited
-            }else if (addr <= 0xFE9F){
-                // OAM 
-            }else if (addr <= 0xFEFF){
-                // Unusable
-            }else if (addr <= 0xFF7F){
-                // IO Registers 
-            }else if (addr <= 0xFFFE){
-                // HRAM
-            }else{
-                // IE Register
-            }
-            break;
-        default:
-            //invalid address
-            break;
-    }
+    switch(mapper(addr)){
+
+    };
 }
 
 void Bus::writeBus(uint16_t addr,uint8_t byte){
-    
+    switch(mapper(addr)){};
+}
+
+Component Bus::mapper(uint16_t addr){
     switch(addr>>12){
         case 0x0:
         case 0x1:
         case 0x2:
         case 0x3:
             //Rom bank 0
-            break;
+            return Component::ROM0;
         case 0x4:
         case 0x5:
         case 0x6:
         case 0x7:
-            //switchable Rom bank 1 to N (managed by cartridge) 
-            break;
+            //switchable Rom bank 1 to N (managed by cartridge)
+            return Component::ROMX; 
         case 0x8:
         case 0x9:
             //Vram (DMG:has only bank,CGB:can switch BtW 0/1)
-            break;
+            return Component::VRAM;
         case 0xA:
         case 0xB:
             //External Ram from cartridge(switchable)
-            break;
+            return Component::CART_RAM;
         case 0xC:
-            //Work Ram bank 0   (Wram Partition only applicable to CGB)  
+            //Work Ram bank 0   (Wram Partition only applicable to CGB) 
+            return Component::WRAM; 
         case 0xD:
             //Work Ram bank 1-7 (Wram Partition only applicable to CGB)
-            break;
+            return Component::WRAM;
         case 0xE://upto FDFF
             //Echo RAM (mirror of C000–DDFF) prohibited
-            break;
+            return Component::ECHO_RAM;
         case 0xF:
             if (addr <= 0xFDFF){
                 // Echo RAM (mirror of C000–DDFF) prohibited
+                return Component::ECHO_RAM;
             }else if (addr <= 0xFE9F){
                 // OAM 
+                return Component::OAM;
             }else if (addr <= 0xFEFF){
                 // Unusable
+                return Component::INVALID;
             }else if (addr <= 0xFF7F){
                 // IO Registers 
+                return Component::IOreg;
             }else if (addr <= 0xFFFE){
                 // HRAM
+                return Component::HRAM;
             }else{
                 // IE Register
+                return Component::IEreg;
             }
             break;
         default:
             //invalid address
+            return Component::INVALID;
             break;
     }
 }
--- src/core/bus/Bus.hpp
+++ src/core/bus/Bus.hpp
@@ -1,9 +1,25 @@
 #include <cstdint>
 
+enum class Component:uint8_t{
+    ROM0,
+    ROMX,
+    VRAM,
+    CART_RAM,
+    WRAM,
+    //SWRAM,
+    ECHO_RAM,
+    OAM,
+    IOreg,
+    HRAM,
+    IEreg,
+    INVALID=0xFF,
+};
+
 class Bus{
     public:
         uint8_t readBus(uint16_t addr);
         void writeBus(uint16_t addr,uint8_t byte);
     private:
+        Component mapper(uint16_t addr);
         //components
 };
